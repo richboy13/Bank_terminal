0d2275b4f4f9ec713d7af1a23f986093147162d7
nv_master <richboy13@mail.ru>
2024-03-03 23:21:10 +0500

    добавил выбор в меню регистрацию и вход

--- bank.py
+++ bank.py
@@ -1,29 +1,34 @@
 accounts = {}  # Словарь для хранения информации об учетных записях
+_login = "zero"
 
-
-def main_menu(login):
+def main_menu():
     """
     Отображает главное меню и обрабатывает выбор пользователя.
     """
-    print(f'\nWelcome, {login}')
-    is_active = True
-    while is_active:
-        print('\n1. Просмотр баланса')
-        print('2. Депозит средств.')
-        print('3. Перевод средств между счетами')
-        print('4. История транзакций')
-        print('5. Выход')
-        action = int(input('\nВыберите действие (1, 2, ...): '))
-        if action == 1:
-            view_balance(login)
-        if action == 2:
-            deposit(login)
-        if action == 3:
-            transfer(login)
-        if action == 4:
-            transaction_history(login)
-        if action == 4:
-            logout()
+    print('\n1. Создать аккаунт')
+    print('2. Войти в личный кабинет.')
+    action = int(input('\nВыберите действие (1 или 2): '))
+    if action == 1:
+        create_account()
+    if action == 2:
+        is_active = login()
+        while is_active:
+            print('\n1. Просмотр баланса')
+            print('2. Депозит средств.')
+            print('3. Перевод средств между счетами')
+            print('4. История транзакций')
+            print('5. Выход')
+            action = int(input('\nВыберите действие (1, 2, ...): '))
+            if action == 1:
+                view_balance(login)
+            if action == 2:
+                deposit(login)
+            if action == 3:
+                transfer(login)
+            if action == 4:
+                transaction_history(login)
+            if action == 4:
+                logout()
 
 
 
@@ -35,18 +40,22 @@ def create_account():
     login = input('Enter login: ')
     password = input('Enter password: ')
     accounts[login] = password
+    print(len(accounts))
 
 
 def login():
     """
     Вход в существующую учетную запись.
     """
-    login = input('Enter login: ')
+    _login = input('Enter login: ')
     password = input('Enter password: ')
-    if accounts[login] == password:
-        main_menu(login)
+    if accounts[_login] == password:
+        main_menu(_login)
+        return True
     else:
         print('Wrong login or password')
+        return False
+
 
 
 def view_balance(account_id):
@@ -86,3 +95,4 @@ def logout():
 
 if __name__ == "__main__":
     main_menu()
+
